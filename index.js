c42105f03e0b2e64e32fc91374e085a2cc4afa47
Nurina <nurina.junus@gmail.com>
2021-05-22 15:06:53 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// declare + assign currentUser
+let currentUser = 'Nurina';
+
+// declare welcomeMessage
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+
+// declare excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// declare shortGreeting
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
